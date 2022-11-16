7a2ae224e9c68761901075e8ea11ec1b5c7f076e
scardinanoire <andre.corsi@outlook.it>
2022-11-16 15:39:34 +0100

    inizio

--- sketch.js
+++ sketch.js
@@ -1,16 +1,7 @@
-function preload() {
-	// put preload code here
-}
-
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	// put setup code here
-	const message = "This is a template reposotory\nfor the course elective Creative Coding\nCommunication Design, Politecnico di Milano";
-	textAlign(CENTER, CENTER);
-  textSize(16)
-	text(message, width/2, height/2);
+  createCanvas(400, 400);
 }
 
 function draw() {
-	// put drawing code here
+  background(220);
 }
