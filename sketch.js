cb139e71f52d6210c32525d57d41be0b3a6c1d9d
scardinanoire <andre.corsi@outlook.it>
2022-11-16 16:45:53 +0100

    pallino che si muove bene

--- sketch.js
+++ sketch.js
@@ -1,7 +1,51 @@
+let x = 0;
+let y = 0;
+let vx = 0;
+let vy = 0;
+let diam = 10;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  textSize(10);
+
+  x = width / 2;
+  y = height / 2;
 }
 
 function draw() {
-  background(220);
+  background(255);
+  fill(0);
+
+  circle(x, y, diam);
+
+  vx += rotationY / 400;
+  vy += rotationX / 400;
+
+  x += vx;
+  y += vy;
+
+  if (x < diam / 2) {
+    x = diam / 2;
+    vx = 0;
+  } else if (x > width - diam / 2) {
+    x = width - diam / 2;
+    vx = 0;
+  }
+
+  if (y < diam / 2) {
+    y = diam / 2;
+    vy = 0;
+  } else if (y > height - diam / 2) {
+    y = height - diam / 2;
+    vy = 0;
+  }
+
+  text("rX: " + round(rotationX), 0, 50);
+  text("rY: " + round(rotationY), 0, 60);
+
+  text("X: " + x, 0, 80);
+  text("Y: " + y, 0, 90);
+
+  text("width: " + width, 0, 110);
+  text("height: " + height, 0, 120);
 }
